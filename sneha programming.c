cc152a3c3690496b92eed290fbe70823db709712
SNEHA SHARMA <snehasharma50396@gmail.com>
2025-08-28 11:52:02 +0530

    Add files via upload

---
+++ sneha programming.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+int main()
+{
+    int num1, num2, sum;
+    // ask the user for input
+    printf("enter first number:");
+    scanf("%d",&num2);
+
+    printf("Enter second number:");
+    scanf("%d", &num2);
+
+    //calculate the sum
+    sum = num1+num2;
+    // print the result
+    printf("The sum of %d and %d is %d\n", num1,num2,sum);
+
+}
