51b00ae4640df7875a3ca7d0907088b619c59d96
SNEHA SHARMA <snehasharma50396@gmail.com>
2025-08-28 12:30:20 +0530

    Add files via upload

---
+++ To apply Arithmetic operator onto integers.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main()
+{
+    int A,B;
+    printf("Enter first integer: ");
+    scanf("%d", &A);
+    printf("Enter second integer: ");
+    scanf("%d", &B);
+    printf("\nArithmetic operations on %d and %d:\n",A,B);
+    printf("Addition:%d\n",A+B);
+    printf("Subtraction:%d\n",A-B);
+    printf("multiplication:%d\n",A*B);
+    printf("Division: %d\n",A/B);
+}
+
+
